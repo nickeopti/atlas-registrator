2d031ec54b997a9e9725ab3ba359a2ea389f06e1
Nicklas Boserup <nibos@di.ku.dk>
2023-07-15 20:57:07 +0200

    Visualise training progress

---
+++ vis.R
@@ -0,0 +1,34 @@
+#!/usr/bin/env Rscript
+
+library(readr)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+args <- commandArgs(trailingOnly = TRUE)
+
+v_num <- args[1]
+metrics_path <- paste(
+    "logs",
+    "lightning_logs",
+    paste("version_", v_num, sep = ""),
+    "metrics.csv",
+    sep = "/"
+)
+
+metrics <- read_csv(metrics_path)
+
+metrics %>%
+    pivot_longer(!c(epoch, step), names_to = "loss", values_to = "value") %>%
+    group_by(epoch, loss) %>%
+    summarise(value = mean(value)) %>%
+    ggplot() +
+        aes(x = epoch, y = value) +
+        facet_wrap(~loss, scales = "free_y") +
+        ylim(0, NA) +
+        geom_line() +
+        geom_smooth() +
+        xlab("Epoch") +
+        ylab("Loss")
+
+ggsave(paste("plots", paste(args[1], ".png", sep = ""), sep = "/"), width = 15, height = 8, units = "cm", dpi = 600)
